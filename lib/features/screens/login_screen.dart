b6810359f8118107917bdcf38c4a80f10dbb225e
Seonyoung Pang <sunyeongbang98@gmail.com>
2025-01-03 21:16:17 +0900

    update login screen

--- lib/features/screens/login_screen.dart
+++ lib/features/screens/login_screen.dart
@@ -12,18 +12,55 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset('assets/images/logo.png'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'Skip',
-              style: TextStyle(color: Pallete.blueColor),
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/images/logo.png',
+            width: 30,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Skip',
+                style: TextStyle(color: Pallete.blueColor),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
             ),
-          )
-        ],
-      ),
-    );
+            Image.asset(
+              'assets/images/loginEmote.png',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/google.png',
+                width: 20,
+              ),
+              label: Text('Continue with Google'),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 40,
+                ),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                    color: Pallete.greyColor,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
