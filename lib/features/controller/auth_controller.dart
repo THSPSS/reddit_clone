0a56a4016697ff67686f4f9e27b19ee7152fa59f
Seonyoung Pang <sunyeongbang98@gmail.com>
2025-01-09 12:00:28 +0900

    update add snackbar

--- lib/features/controller/auth_controller.dart
+++ lib/features/controller/auth_controller.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/uitils.dart';
 import 'package:reddit_clone/features/repository/auth_repository.dart';
 
 //instead of this code
@@ -18,7 +20,9 @@ class AuthController {
     required AuthRepository authRepository,
   }) : _authRepository = authRepository;
 
-  void signInWithGoogle() {
-    _authRepository.signInWithGoogle();
+  void signInWithGoogle(BuildContext context) async {
+    final user = await _authRepository.signInWithGoogle();
+    //catch error l:failure , r:success
+    user.fold((l) => showSnackBar(context, l.message), (r) => null);
   }
 }
--- lib/features/repository/auth_repository.dart
+++ lib/features/repository/auth_repository.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_provider.dart';
 import 'package:reddit_clone/models/user_model.dart';
 
 import '../../core/constants/constants.dart';
+import '../../core/type_defs.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -34,7 +37,7 @@ class AuthRepository {
   CollectionReference get _users =>
       _firestore.collection(FirebaseConstants.usersCollection);
 
-  void signInWithGoogle() async {
+  FutureEither<UserModel> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
@@ -45,23 +48,32 @@ class AuthRepository {
         idToken: googleAuth?.idToken,
       );
 
+      late UserModel userModel;
+
       //save on firebase console
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
 
-      UserModel userModel = UserModel(
-        name: userCredential.user!.displayName ?? 'untitled',
-        profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
-        banner: Constants.bannerDefault,
-        uid: userCredential.user!.uid,
-        isAuthenticated: true,
-        karma: 0,
-        awards: [],
-      );
-
-      await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      //check if new user or not
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        userModel = UserModel(
+          name: userCredential.user!.displayName ?? 'untitled',
+          profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+          banner: Constants.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: [],
+        );
+        await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      }
+      //when process is successful
+      return right(userModel);
+    } on FirebaseException catch (e) {
+      //throw to next catch block
+      throw e.message!;
     } catch (e) {
-      print('auth Repository Error : $e');
+      return left(Failure(e.toString()));
     }
   }
 }
