9f8e84e20f147dc559914e1dbdcb4124870cf2da
Seonyoung Pang <sunyeongbang98@gmail.com>
2025-01-09 14:18:44 +0900

    update state to check persistence of user data through out whole app

--- lib/features/controller/auth_controller.dart
+++ lib/features/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/uitils.dart';
@@ -18,6 +19,16 @@ final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   ),
 );
 
+final authStateChangeProvider = StreamProvider((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.authStateChange;
+});
+
+final getUserDataProvider = StreamProvider.family((ref, String uid) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(uid);
+});
+
 //stateNotifier will inform to ui so that it can changed the loading state ui
 class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
@@ -30,6 +41,8 @@ class AuthController extends StateNotifier<bool> {
         //loading state
         super(false);
 
+  Stream<User?> get authStateChange => _authRepository.authStateChange;
+
   void signInWithGoogle(BuildContext context) async {
     state = true;
     final user = await _authRepository.signInWithGoogle();
@@ -39,5 +52,9 @@ class AuthController extends StateNotifier<bool> {
       (userModel) =>
           _ref.read(userProvider.notifier).update((state) => userModel),
     );
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _authRepository.getUserData(uid);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/controller/auth_controller.dart';
+import 'package:reddit_clone/models/user_model.dart';
+import 'package:reddit_clone/router.dart';
+import 'package:routemaster/routemaster.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:reddit_clone/features/screens/login_screen.dart';
-
 import 'theme/pallete.dart';
 
 void main() async {
@@ -18,16 +23,70 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: Pallete.darkModeAppTheme,
-      home: const LoginScreen(),
-    );
+    return ref.watch(authStateChangeProvider).when(
+          data: (data) => MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Pallete.darkModeAppTheme,
+            routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+              if (data != null) {
+                getData(ref, data);
+                if (userModel != null) {
+                  return loggedInRoute;
+                }
+              }
+              return loggedOutRoute;
+            }),
+            routeInformationParser: RoutemasterParser(),
+          ),
+          error: (error, s) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
+
+// class MyApp extends ConsumerWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return ref.watch(authStateChangeProvider).when(
+//           data: (data) => MaterialApp.router(
+//             debugShowCheckedModeBanner: false,
+//             title: 'Flutter Demo',
+//             theme: Pallete.darkModeAppTheme,
+//             routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+//               if (data != null) {
+//                 return loggedInRoute;
+//               } else {
+//                 return loggedOutRoute;
+//               }
+//             }),
+//             routeInformationParser: RoutemasterParser(),
+//           ),
+//           error: (error, s) => ErrorText(error: error.toString()),
+//           loading: () => const Loader(),
+//         );
+//   }
+// }
